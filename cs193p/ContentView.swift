f85b73b5ff3ad23ac70938a72020bf6af3233f7c
Dmytro Kulakov <kulakovdmitr@gmail.com>
2024-08-31 13:21:33 +0200

    Add base cards loop

--- cs193p/ContentView.swift
+++ cs193p/ContentView.swift
@@ -9,15 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Cards()
+    }
+}
+
+struct Cards: View {
+    var body: some View {
+        Grid {
+            ForEach(1...5, id: \.self) {_ in
+                Card().frame(width: 100)
+            }
+           
         }
         .padding()
     }
 }
+
+struct Card: View {
+    var body: some View {
+        ZStack(alignment: /*@START_MENU_TOKEN@*/Alignment(horizontal: .center, vertical: .center)/*@END_MENU_TOKEN@*/ ) {
+            RoundedRectangle(cornerSize: /*@START_MENU_TOKEN@*/CGSize(width: 20, height: 10)/*@END_MENU_TOKEN@*/).fill(.cyan)
+            RoundedRectangle(cornerSize: /*@START_MENU_TOKEN@*/CGSize(width: 20, height: 10)/*@END_MENU_TOKEN@*/).fill(.white).padding(4)
+            Text("🥰").font(.largeTitle)
+        }
+        Spacer()
+    }
+}
+
+
+
 
 #Preview {
     ContentView()
