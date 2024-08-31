1fd8a266b036162a6b9066bf1a7913f01ba8cda5
Dmytro Kulakov <kulakovdmitr@gmail.com>
2024-08-31 11:38:24 +0200

    Initial Commit

---
+++ cs193p/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cs193p
+//
+//  Created by Binary Studio on 31.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cs193p/cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193pApp.swift
+//  cs193p
+//
+//  Created by Binary Studio on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
